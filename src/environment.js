cb815d2418972d253dbd184cedd7c165b6dfa885
sarumjanuch <sarumjanuch@gmail.com>
2018-11-06 22:02:49 +0200

    New Reconnection Mechanism

--- src/environment.js
+++ src/environment.js
@@ -4,7 +4,7 @@ var extend = require('util')._extend;
 extend(exports, require('./environment_browser'));
 
 extend(exports, {
-  WebSocket: require('ws'),
+  WebSocket: window.WebSocket,
   Promise: exports.Promise || require('promiscuous'),
   console: require('console'),
   timers: require('timers')
